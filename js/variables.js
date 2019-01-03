837f1286002af84f9c77ea6bdf5632ba2bd4f47f
Evan Wright <rideburtonsnow3@gmail.com>
2019-01-03 16:11:21 -0700

    adding code

--- js/variables.js
+++ js/variables.js
@@ -1,14 +1,67 @@
-var firstName = 'John';
-console.log(firstName)
+/****************************
+ * Variables and data types
+ */
 
-var lastName = 'Smith';
+// var firstName = 'John';
+// console.log(firstName)
+
+// var lastName = 'Smith';
+// var age = 28;
+
+// var fullAge = true; 
+// console.log(fullAge);
+
+// var job;
+// console.log(job)
+
+// job = 'Teacher';
+// console.log(job);
+
+
+/*Variable names rules*/
+
+//cannnot start a variabel with a number
+//example: var 3years = 3
+
+//cannnot start a variable wiht a symble except for an underscore _ and dollar sign $
+//example: var #heavy = 'heavy' 
+
+//cannot have a symbple in the middle of the variable name either
+//example: var john&Mark = 'John and Mark'; 
+
+//cannnot used reserved JavaScript keywords as variable names
+//example: var function = 23;
+
+
+/****************************
+ * Variables mutation and type coercion 
+ */
+
+var firstName = 'John';
 var age = 28;
 
-var fullAge = true; 
-console.log(fullAge);
+//Type coercion
+console.log(firstName + ' ' + age)
 
-var job;
-console.log(job)
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
 
-job = 'Teacher';
-console.log(job);
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+//Variable mutation
+age = 'twenty eight';
+job = 'driver';
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last name?');
+console.log(firstName + ' ' + lastName);
+
+
+
+
+
+
+
+
+
