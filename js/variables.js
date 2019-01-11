40364fc50bf6ac9eb22102c9885f4dbd20c57d8e
Evan Wright <rideburtonsnow3@gmail.com>
2019-01-11 15:37:01 -0700

    nest lesson

--- js/variables.js
+++ js/variables.js
@@ -37,25 +37,30 @@
  * Variables mutation and type coercion 
  */
 
-var firstName = 'John';
-var age = 28;
+// var firstName = 'John';
+// var age = 28;
 
-//Type coercion
-console.log(firstName + ' ' + age)
+// //Type coercion
+// console.log(firstName + ' ' + age)
 
-var job, isMarried;
-job = 'teacher';
-isMarried = false;
+// var job, isMarried;
+// job = 'teacher';
+// isMarried = false;
 
-console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+// console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
 
-//Variable mutation
-age = 'twenty eight';
-job = 'driver';
-alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+// //Variable mutation
+// age = 'twenty eight';
+// job = 'driver';
+// alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
 
-var lastName = prompt('What is his last name?');
-console.log(firstName + ' ' + lastName);
+// var lastName = prompt('What is his last name?');
+// console.log(firstName + ' ' + lastName);
+
+/**************************
+ * Basic Operators
+ */
+
 
 
 
