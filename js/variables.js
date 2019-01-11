e9bee393b6c12b492d161fb24d546eaba912e851
Evan Wright <rideburtonsnow3@gmail.com>
2019-01-11 15:50:21 -0700

    adding code, get rid of CSS

--- js/variables.js
+++ js/variables.js
@@ -57,12 +57,31 @@
 // var lastName = prompt('What is his last name?');
 // console.log(firstName + ' ' + lastName);
 
+
 /**************************
  * Basic Operators
  */
 
+var year, yearJohn, yearMark;
+now = 2018
+ageJohn = 28;
+ageMark = 33;
 
+//Math operators 
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
+console.log(yearJohn)
 
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10); 
+
+//Logical oporators
+var johnOlder = ageJohn < ageMark;
+console.log(johnOlder)
+
+//Typeof operator
+console.log(typeof johnOlder);
 
 
 
