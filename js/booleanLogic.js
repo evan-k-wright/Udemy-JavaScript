70d0134bce501dbfaf57b16a66574c161d9f19bd
Evan Wright <rideburtonsnow3@gmail.com>
2019-01-18 15:08:52 -0700

    learning javascript

--- js/booleanLogic.js
+++ js/booleanLogic.js
@@ -2,4 +2,13 @@
  * Boolean logic
  */
 
- 
+ var firstName = 'John';
+ var age = '16';
+
+ if (age < 13) {
+     console.log(firstName + ' is a boy.');
+ } else if () { //between 13 and 20 >= 13 AND (&&) < 20
+    console.log(firstName + ' is a teenager.');
+ } else { 
+     console.log(firstName + ' is a man.');
+ }
