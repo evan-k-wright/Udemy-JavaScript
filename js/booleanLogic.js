10df3db589961446260dad314462cd14904b8fa4
Evan Wright <rideburtonsnow3@gmail.com>
2019-01-28 15:21:20 -0700

    learning js

--- js/booleanLogic.js
+++ js/booleanLogic.js
@@ -2,21 +2,45 @@
  * Boolean logic
  */
 
- var firstName = 'John';
- var age = '26';
+//  var firstName = 'John';
+//  var age = '26';
 
- if (age < 13) {
-     console.log(firstName + ' is a boy.');
- } else if (age >= 13 && age  < 20 ) { //between 13 and 20 >= 13 AND (&&) < 20
-    console.log(firstName + ' is a teenager.');
- } else if(age >= 20 && age < 30) {
-     console.log(firstName + ' is a young man.');
- } else { 
-     console.log(firstName + ' is a man.');
- }
+//  if (age < 13) {
+//      console.log(firstName + ' is a boy.');
+//  } else if (age >= 13 && age  < 20 ) { //between 13 and 20 >= 13 AND (&&) < 20
+//     console.log(firstName + ' is a teenager.');
+//  } else if(age >= 20 && age < 30) {
+//      console.log(firstName + ' is a young man.');
+//  } else { 
+//      console.log(firstName + ' is a man.');
+//  }
 
+/***************************
+ * The Ternary Operator and Switch Statements
+ */
 
+ //ternary operator
+ var firstName = "John";
+ var age = 16;
+
+ age >= 18 ? console.log(firstName + ' drinks beer.') 
+ : console.log(firstName + ' drinks juice.');
+
+ var drink = age >= 18 ? 'beer' : 'juice';
+ console.log(drink);
  
+/*if (age >=18) {
+    var drink = 'beer';
+} else {
+    var drink = 'juice';
+}*/
+
+//switch statement
+
+
+
+
+
 
 // var car = 'Mazda'
 // var Mazda = 'red'
